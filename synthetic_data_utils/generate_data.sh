6a78144d36f07bc0f2f38bb08480fb7245f2d68c
preritj <prerit.jaiswal@gmail.com>
2018-10-19 10:24:57 -0700

    init

--- synthetic_data_utils/generate_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-INPUT_DIR=/media/storage/datasets/products/MasterCard/out_final/objects
-OUTPUT_DIR=/media/storage/datasets/products/TrainData/train_data/sample_007
-
-python3 dataset_generator.py $INPUT_DIR $OUTPUT_DIR --dontocclude --add_distractors --selected
